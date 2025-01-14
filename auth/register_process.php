42e3e1a6842a5bd702c0b3930f0642e3cc9f67bc
anandanurokta <ananda.nur1411@smk.belajar.id>
2025-01-14 12:14:50 +0700

    register proses

--- auth/register_process.php
+++ auth/register_process.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         } else {
             $_SESSION['notification'] = [
                 'type' => 'danger',
-                'message' => 'Gagal Registrasi: ' . mysqli_error($conn)   
+                'message' => 'Gagal Registrasi: ' . mysqli_error($conn) 
             ];
         }
         header('Location: login.php');
