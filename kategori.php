c6a671f060bed39d810061a215410e44c9e10e64
anandanurokta <ananda.nur1411@smk.belajar.id>
2025-01-20 13:59:04 +0700

    proses kategori

--- kategori.php
+++ kategori.php
@@ -42,7 +42,7 @@ include '.includes/toast_notification.php';
     <!--Dropdown untuk opsi edit dan delete-->
     <div class="dropdown">
         <button type="button" class="btn p-0 dropdown-toggle 
-         hide-arrow" data-bs-toggel"dropdown">
+         hide-arrow" data-bs-toggel "dropdown">
         <i class="bx bx-dots-vertical-rounded"></i>
     </button>
     <div class="dropdown-menu">
@@ -56,7 +56,29 @@ include '.includes/toast_notification.php';
   </div>
 </td>
                     </tr>
-                    <!-- Modal untuk hapus kategori-->
+                    <!-- Modal untuk Hapus Data Kategori -->
+<div class="modal fade" id="deleteCategory_<?= $category['category_id']; ?>" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Hapus Kategori?</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+      <div class="modal-body">
+        <form action="proses_kategori.php" method="POST">
+          <div>
+            <p>Tindakan ini tidak bisa dibatalkan.</p>
+            <input type="hidden" name="catID" value="<?= $category['category_id']; ?>">
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Batal</button>
+            <button type="submit" name="delete" class="btn btn-primary">Hapus</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 
                     <!-- Modal untuk update kategori-->
 
@@ -90,7 +112,7 @@ include '.includes/toast_notification.php';
            <button type="submit" name="simpan" class="btn btn-primary">Simpan</button>
           </div>
         </form>
-      </div>
+      </div> 
     </div>
   </div>
 </div>
