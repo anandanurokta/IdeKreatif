e9ff69bd7ad1db265c268e15046e9ee134aefcbe
anandanurokta <ananda.nur1411@smk.belajar.id>
2025-01-21 15:22:35 +0700

    login

--- auth/login_auth.php
+++ auth/login_auth.php
@@ -11,37 +11,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
-       
+        // Verifikasi password
         if (password_verify($password, $row["password"])) {
             $_SESSION["username"] = $username;
             $_SESSION["name"] = $row["name"];
             $_SESSION["role"] = $row["role"];
             $_SESSION["user_id"] = $row["user_id"];
-            
+            // Set notifikasi selamat datang
             $_SESSION['notification'] = [
                 'type' => 'primary',
-                'message' => 'Selamat Datang Kembali!'
+                'message' => 'Selamat datang kembali!'
             ];
-            
+            // Redirect ke dashboard
             header('Location: ../dashboard.php');
             exit();
         } else {
-            
+            // Password salah
             $_SESSION['notification'] = [
                 'type' => 'danger',
                 'message' => 'Username atau Password salah'
             ];
         }
     } else {
-
+        // Username tidak ditemukan
         $_SESSION['notification'] = [
             'type' => 'danger',
             'message' => 'Username atau Password salah'
         ];
     }
-    
+    // Redirect kembali ke halaman login jika gagal
     header('Location: login.php');
     exit();
-}
-$conn->close();
-?>
+    }
+    $conn->close();
+    ?>
+         
